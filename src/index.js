ff00dda6ec98a985df1400031f1a91e920c5f024
Nico <nicolas.opazo2001@alumnos.ubiobio.cl>
2024-11-13 19:47:33 -0300

    ADD setup inicial del servidor

---
+++ src/index.js
@@ -0,0 +1,47 @@
+'use strict';
+import cors from 'cors';
+import morgan from 'morgan';
+import express, { json, urlencoded } from 'express';
+
+import indexRoutes from './routes/index.routes.js';
+import { HOST, PORT } from './config/configEnv.js';
+
+async function setupServer() {
+  try {
+    const app = express();
+
+    app.disable('x-powered-by');
+
+    app.use(
+      cors({
+        credentials: true,
+        origin: true,
+      }),
+    );
+
+    app.use(
+      json({
+        limit: '1mb',
+      }),
+    );
+
+    app.use(morgan('dev'));
+
+    app.use(
+      urlencoded({
+        extended: true,
+        limit: '1mb',
+      }),
+    );
+
+    app.use('/api', indexRoutes);
+
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en ${HOST}:${PORT}/api`);
+    });
+  } catch (error) {
+    console.log('Error en index.js -> setupServer(), el error es: ', error);
+  }
+}
+
+setupServer();
