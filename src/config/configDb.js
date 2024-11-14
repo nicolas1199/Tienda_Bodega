9e9460c837bf836efdfe15b7e0b36b53ab82711e
Nico <nicolas.opazo2001@alumnos.ubiobio.cl>
2024-11-13 22:24:52 -0300

    ADD export default para la cofiguracion de la DB

--- src/config/configDb.js
+++ src/config/configDb.js
@@ -11,9 +11,10 @@ export const db = mysql.createPool({
   host: DB_HOST,
   port: DB_PORT,
   user: DB_USERNAME,
-  password: DB_PASSWORD,
   database: DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
+
+export default db;
