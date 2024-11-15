ec5fe872fb3d31226979240cd4630a5ca7344c75
Gabo288 <elgabrieljeres@gmail.com>
2024-11-15 18:57:52 -0300

    modified:   src/controllers/general.controller.js

--- src/controllers/general.controller.js
+++ src/controllers/general.controller.js
@@ -6,27 +6,20 @@ import { Usuarios } from "../models/usuarios.model.js"
 import { Ventas } from "../models/ventas.model.js";
 
 export const general = {
-    getAll: (req, res) => {
-        Usuarios.getAll().then((usr, err) => {
-            Materiales.getAll().then((mat) => {
-                Categorias.getAll().then((cat) => {
-                    Carritos.getAll().then((car) => {
-                        Boletas.getAll().then((bol) => {
-                            Ventas.getAll().then((ven)=>{
-                                res.render((req.params.tabla), {
-                                    usuarios: usr,
-                                    materiales: mat,
-                                    categorias: cat,
-                                    carrito:car,
-                                    boletas:bol,
-                                    ventas:ven
-                                });//nota: modificar para renderizar
-                            })
-                        })
-                    })
-                })
-            })
-        })
+    getAll: async (req, res) => {
+        const usr = await Usuarios.getAll()
+        const mat = await Materiales.getAll()
+        const cat = await Categorias.getAll()
+        const car = await Carritos.getAll()
+        const bol = await Boletas.getAll()
+        const ven = await Ventas.getAll()
+        res.render((req.params.tabla), {
+            usuarios: usr,
+            materiales: mat,
+            categorias: cat,
+            carrito: car,
+            boletas: bol,
+            ventas: ven
+        });
     }
-    
 }
