827967bfa00368e4c1821f7638b8830ae9189fbb
Nico <nicolas.opazo2001@alumnos.ubiobio.cl>
2024-11-15 18:31:56 -0300

    ADD funciones para actualizar y eliminar categorías, y mejora en la respuesta de creación y obtención de categorías

--- src/controllers/categorias.controller.js
+++ src/controllers/categorias.controller.js
@@ -7,8 +7,10 @@ export const createCategoria = async (req, res) => {
     const newCategoria = {
       nombre_categoria: nombre,
     };
+
     const id = await Categorias.create(newCategoria);
-    res.status(201).json({ id });
+
+    res.status(201).json({ message: 'Categoría creada', id });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
@@ -17,16 +19,51 @@ export const createCategoria = async (req, res) => {
 export const getCategorias = async (req, res) => {
   try {
     const categorias = await Categorias.getAll();
+
+    if (categorias.length === 0) {
+      return res.status(404).json({ error: 'No hay categorías' });
+    }
+
     res.status(200).json(categorias);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
-export const getCategoriaById = async (req, res) => {
+export const updateCategoria = async (req, res) => {
   try {
-    const categoria = await Categorias.getById(req.params.id);
-    res.status(200).json(categoria);
+    const { nombre } = req.body;
+    const id = req.params.id;
+
+    const categoria = await Categorias.getById(id);
+
+    if (!categoria) {
+      return res.status(404).json({ error: 'La categoría no existe' });
+    }
+
+    const updatedCategoria = {
+      nombre_categoria: nombre,
+    };
+
+    await Categorias.update(id, updatedCategoria);
+    res.status(204).end();
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const deleteCategoria = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const categoria = await Categorias.getById(id);
+
+    if (!categoria) {
+      return res.status(404).json({ error: 'La categoría no existe' });
+    }
+
+    await Categorias.delete(id);
+
+    res.status(204).end();
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
