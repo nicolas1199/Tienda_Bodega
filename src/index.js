65e03fe1f17bead617c82db3be4a3d001470364e
Roberto <roberto.leyva2301@alumnos.ubiobio.clqgit status:qq>
2024-12-06 01:40:41 -0300

    cambio en index.js

--- src/index.js
+++ src/index.js
@@ -18,11 +18,6 @@
 
   const MySQLStoreImport = MySQLStore(session);
 
-<<<<<<< HEAD
-  async function setupServer() {
-    try {
-      const app = express();
-=======
 const options = {
   host: DB_HOST,
   port: DB_PORT,
@@ -38,7 +33,6 @@ export const sessionStore = new MySQLStoreImport({}, connection);
 async function setupServer() {
   try {
     const app = express();
->>>>>>> b511a7ce1c424ffd9bd0f1d7bb02617d83ecfa0d
 
       app.disable('x-powered-by');
 
@@ -57,34 +51,6 @@ async function setupServer() {
 
       app.use(morgan('dev'));
 
-<<<<<<< HEAD
-      const options = {
-        host: DB_HOST,
-        port: DB_PORT,
-        user: DB_USERNAME,
-        password: DB_PASSWORD || '', // Forzamos el uso de cadena vacía si es necesario
-        database: DATABASE,
-      };
-
-      const connection = await mysql.createConnection(options);
-
-      const sessionStore = new MySQLStoreImport({}, connection);
-
-      app.use(
-        session({
-          secret: 'Super Secreto',
-          saveUninitialized: false,
-          resave: false,
-          store: sessionStore,
-          httpOnly: false,
-          cookie: {
-            maxAge: 600000 * 60,
-            secure: false,
-          },
-          domain: 'localhost',
-        }),
-      );
-=======
     app.use(
       session({
         name: 'sessionID',
@@ -100,7 +66,6 @@ async function setupServer() {
         },
       }),
     );
->>>>>>> b511a7ce1c424ffd9bd0f1d7bb02617d83ecfa0d
 
       app.use(
         urlencoded({
